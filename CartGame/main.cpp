c80fe20f6adafd341d22340070dd9f928a699416
Кирилл Рыжков <kirilua38@icloud.com>
2018-01-14 18:18:35 +0200

    Initial Commit

---
+++ CartGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CartGame
+//
+//  Created by Кирилл  on 14.01.2018.
+//  Copyright © 2018 Кирилл . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
